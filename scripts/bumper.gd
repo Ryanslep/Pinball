53c985d7aa06ee3e455c8c7275e38d8bf99208bc
Ryanslep <ryan.slep@gmail.com>
2025-10-25 13:50:37 -0400

    bumper added

--- scripts/bumper.gd
+++ scripts/bumper.gd
@@ -1,9 +1,23 @@
 extends StaticBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var impulse_strength: float = 800.0
+@export var bump_strength: float = 1000.0
+@export var bump_cooldown: float = 0.3 #Seconds between bumps
 
-func _on_area_entered(area: Area2D) -> void:
-	var body := area.get_parent() as RigidBody2D
-	if body:
+var can_bump: bool
+
+func _ready() -> void:
+	can_bump = true
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body and can_bump:
 		var dir := (body.global_position - global_position).normalized()
-		body.apply_impulse(dir * impulse_strength)
+		body.apply_impulse(dir * bump_strength)
+		can_bump = false
+		# Simple cooldown
+		get_tree().create_timer(bump_cooldown).connect("timeout", Callable(self, "_on_bump_reset"))
+		animated_sprite_2d.play("Bump")
+
+func _on_bump_reset() -> void:
+	can_bump = true
